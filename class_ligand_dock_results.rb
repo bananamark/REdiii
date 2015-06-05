f57ec1797b852f1175a3f1f3a7a669ce4c22d080
bananamark <markus.f.bohn@gmail.com>
2015-06-05 14:45:44 -0700

    Create class_ligand_dock_results.rb

---
+++ class_ligand_dock_results.rb
@@ -0,0 +1,26 @@
+class Ligand_Dock_Results
+  def contents
+    File.open("output_files/liganded_pdb.updated_refine_001.log") { |f| f.read }
+  end
+
+  def statistics
+    prefilter = contents.match(/phenix.refine: finished ===========================(.*)/m)[1].strip #extracting the final statistics table
+    statistics = prefilter.scan(/(\d+(?:\.\d+)?)/) #converting table to array
+  end
+
+  def r_start
+    return statistics[-3].map(&:to_f)
+  end
+
+  def r_final
+    return statistics[-2].map(&:to_f)
+  end
+
+  def rfree_start
+    return statistics[-3].map(&:to_f)
+  end
+
+  def rfree_final
+    return statistics[-1].map(&:to_f)
+  end
+end
